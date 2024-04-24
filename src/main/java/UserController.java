06d96c7d001c93cd81d0bd6bed0850031f215d57
caoowei <caoowei@atguigu.com>
2024-04-24 15:59:58 +0800

    提交UserController文件,这是第一次提交

---
+++ src/main/java/UserController.java
@@ -0,0 +1,12 @@
+
+public class UserController {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
