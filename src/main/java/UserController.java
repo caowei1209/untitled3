3f40eec7fe616813f1a3e702842798aac862507d
caoowei <caoowei@atguigu.com>
2024-04-24 16:02:33 +0800

    提交UserController文件,这是第三次提交，这是fix-io分支

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -8,5 +8,9 @@ public class UserController {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 }
